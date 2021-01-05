82a525d545910811c1b68da6f3ee99a9642cdb4e
Dhananjay-at-277915323916 <Dhananjay-at-277915323916>
2021-01-05 21:24:34 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Routes from './routes/routes'
+import Home from "./Home";
+import List from "./List";
+import { Redirect, Switch, Route } from "react-router-dom";
+
 
 
 const App = () => {
 
   return (
-      <Routes /> 
+    <>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/List" component={List} />
+      <Redirect to="/" />
+    </Switch>
+  </>
   )
 }
 
