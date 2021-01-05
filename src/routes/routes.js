486729df3acbb3e0649a8de550029ee218ecfeae
Dhananjay-at-277915323916 <Dhananjay-at-277915323916>
2021-01-05 21:10:41 +0530

    added

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,25 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { Router, Switch, Route, Link } from "react-router-dom";
+import { Redirect, Switch, Route, Link } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../App.css";
 import Index from "../components/Index";
 import List from "../components/List";
-import { history } from "../helpers/history";
 
 const Routes = () => {
   return (
-      <Router history={history}>
-        <div>
-  
-          <div className="container mt-4">
-            <Switch>
-              <Route exact path={"/"} component={Index} />
-              <Route exact path={"/List"} component={List} />
-            </Switch>
-          </div>
-        </div>
-      </Router>
+      <>
+      <Switch>
+        <Route exact path="/" component={Index} />
+        <Route exact path="/List" component={List} />
+        <Redirect to="/" />
+      </Switch>
+    </>
     );
   };
 
